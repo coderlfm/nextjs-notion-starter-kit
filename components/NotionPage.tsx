b3aa795899cbafed12fd33c1cb1b2811780fc32b
茂茂 <375642570@qq.com>
2023-05-04 15:10:42 +0800

    feat: 修复代码块不生成的问题

--- components/NotionPage.tsx
+++ components/NotionPage.tsx
@@ -44,7 +44,7 @@ export const formatDate = (  input: string | number) => {
 const Code = dynamic(() =>
   import('react-notion-x/build/third-party/code').then(async (m) => {
     // add / remove any prism syntaxes here
-    await Promise.all([
+    await Promise.allSettled([
       import('prismjs/components/prism-markup-templating.js'),
       import('prismjs/components/prism-markup.js'),
       import('prismjs/components/prism-bash.js'),
