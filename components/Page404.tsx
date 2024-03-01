0949827abba002c2a18610e7fb7c0399e0d61cb9
茂茂 <375642570@qq.com>
2024-03-01 11:58:23 +0800

    feat: 新增 Clarity 用户分析工具

--- components/Page404.tsx
+++ components/Page404.tsx
@@ -5,7 +5,7 @@ import * as types from '@/lib/types'
 import { PageHead } from './PageHead'
 import styles from './styles.module.css'
 
-export const Page404: React.FC<types.PageProps> = ({ site, pageId, error }) => {
+export const Page404: React.FC<types.PageProps> = ({ site }) => {
   const title = site?.name || 'Notion Page Not Found'
 
   return (
@@ -14,24 +14,26 @@ export const Page404: React.FC<types.PageProps> = ({ site, pageId, error }) => {
 
       <div className={styles.container}>
         <main className={styles.main}>
-          <h1>Notion Page Not Found</h1>
+          <h1 className={styles.text}>页面未找到</h1>
 
-          {error ? (
-            <p>{error.message}</p>
+          {/* {error ? (
+            <p className={styles.text}>{error.message}</p>
           ) : (
             pageId && (
-              <p>
+              <p className={styles.text}>
                 Make sure that Notion page &quot;{pageId}&quot; is publicly
                 accessible.
               </p>
             )
-          )}
+          )} */}
 
           <img
             src='/404.png'
             alt='404 Not Found'
             className={styles.errorImage}
           />
+
+          <a className={styles.text} href='/'>返回首页</a>
         </main>
       </div>
     </>
--- components/PageHead.tsx
+++ components/PageHead.tsx
@@ -82,6 +82,17 @@ export const PageHead: React.FC<
       <meta property='og:title' content={title} />
       <meta name='twitter:title' content={title} />
       <title>{title}</title>
+
+      <script type="text/javascript"
+      dangerouslySetInnerHTML={{
+        __html: `
+(function(c,l,a,r,i,t,y){
+    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+})(window, document, "clarity", "script", "l9ofyxr2cg");
+`}}
+    />
     </Head>
   )
 }
